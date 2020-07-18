7dc1bbd3c2ddc5bbb835a91c0b36f344a2d72b27
origamium <onoueorigami@gmail.com>
2020-07-19 05:19:42 +0900

    feat: stateを追加

--- api/slack/oauth.ts
+++ api/slack/oauth.ts
@@ -9,12 +9,12 @@ const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET;
 const slack = (): FastifyInstance => {
     const app = fastify(process.env.NODE_ENV === "development" ? { logger: true } : {});
     app.get("/api/slack/oauth", async (req, res) => {
-        const { code, temp_code } = req.query as { temp_code?: string; code?: string };
+        const { code, temp_code, state } = req.query as { temp_code?: string; code?: string, state?: string };
         if (code) {
             res.status(200);
             res.header("content-type", "application/json");
             res.send(JSON.stringify(code));
-        } else if (temp_code) {
+        } else if (temp_code && state) {
             const slackOAuthAccessRequest = await fetch(
                 "https://slack.com/api/oauth.v2.access",
                 {
@@ -23,6 +23,7 @@ const slack = (): FastifyInstance => {
                         client_id: SLACK_CLIENT_ID,
                         client_secret: SLACK_CLIENT_SECRET,
                         code: temp_code,
+                        state,
                         redirect_uri:
                             "https://tempest-authorizer.origamium.net/api/slack/oauth",
                     }),
