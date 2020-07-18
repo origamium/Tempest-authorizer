47da052bea6442fca9c54d3d98476417a599f7b9
origamium <onoueorigami@gmail.com>
2020-07-19 04:18:56 +0900

    feat: APIの変更

--- api/slack/oauth.ts
+++ api/slack/oauth.ts
@@ -1,25 +1,25 @@
 import { NowRequest, NowResponse } from "@now/node";
 import bent from "bent";
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 
 const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID;
 const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET;
 
-const slackGetOAuthAccess = bent("https://slack.com/api/oauth.access", "GET", "json", 200);
+const slackGetOAuthAccess = bent("https://slack.com/api/oauth.v2.access", "GET", "json", 200);
 
-const slack = (): fastify.FastifyInstance => {
+const slack = (): FastifyInstance => {
     const app = fastify(process.env.NODE_ENV === "development" ? { logger: true } : {});
     app.get("/api/slack/oauth", async (req, res) => {
-        const { query } = req.query;
-        if (query.state) {
+        const { state, code } = req.query as { state?: string; code?: string };
+        if (state) {
             res.status(200);
             res.header("content-type", "application/json");
-            res.send(JSON.stringify(query));
-        } else if (query.access_token) {
+            res.send(JSON.stringify(state));
+        } else if (code) {
             const slackOAuthAccessRequest = await slackGetOAuthAccess("", {
                 client_id: SLACK_CLIENT_ID,
                 client_secret: SLACK_CLIENT_SECRET,
-                code: query.access_token as string,
+                code,
                 redirect_uri: "https://tempest-authorizer.origamium.net/api/slack/oauth",
             });
 
