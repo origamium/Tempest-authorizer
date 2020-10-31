d8c8798d80ead5b2540a204423bbccebf78a0b7d
origamium <onoueorigami@gmail.com>
2020-11-01 01:34:40 +0900

    はい

---
+++ api/mastodon/request_token.ts
@@ -0,0 +1,69 @@
+import { NowRequest, NowResponse } from "@now/node";
+import formurlencoded from "form-urlencoded";
+import fastify, { FastifyInstance } from "fastify";
+import fetch from "node-fetch";
+import { ProviderData, mstdnjp, pawoo } from "./apikeys";
+
+const getProviderData = (provider: string): ProviderData | never => {
+    switch (provider) {
+        case "mstdnjp":
+            return mstdnjp;
+        case "pawoo":
+            return pawoo;
+        default:
+            throw new Error(`unknown provider name: ${provider}`);
+    }
+};
+
+const mastodonRequestToken = (): FastifyInstance => {
+    const app = fastify(process.env.NODE_ENV === "development" ? { logger: true } : {});
+    app.get("/api/mastodon/v1/:provider", async (req, res) => {
+        const providerName = req.params;
+        const { code, scope } = req.body as { code?: string; scope?: string };
+
+        if (typeof code !== "string") {
+            res.status(400);
+            res.send("bad request: parameter is not string");
+            return;
+        }
+
+        if (typeof providerName !== "string") {
+            res.status(400);
+            res.send("bad request: parameter is not string");
+            return;
+        }
+
+        try {
+            const provider = getProviderData(providerName);
+            const result = await fetch(provider.url, {
+                method: "POST",
+                body: JSON.stringify({
+                    client_id: provider.apiKey,
+                    client_secret: provider.apiSecret,
+                    redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+                    code,
+                    scope,
+                    grant_type: "code",
+                }),
+            });
+
+            const json = result.json();
+
+            res.status(200);
+            res.header("content-type", "application/json");
+            res.send(json);
+        } catch (e) {
+            res.status(400);
+            res.send(`bad request: ${e.message}`);
+            return;
+        }
+    });
+    return app;
+};
+
+const mastodonRequestTokenInstance = mastodonRequestToken();
+
+module.exports = async (req: NowRequest, res: NowResponse): Promise<void> => {
+    await mastodonRequestTokenInstance.ready();
+    mastodonRequestTokenInstance.server.emit("request", req, res);
+};
